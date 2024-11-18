9cf87342f33e550b90ea394627dc295055b5d33e
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-18 18:55:29 -0300

    Add ClientType Enums

---
+++ GerenciadorProdutoECliente/Enums/ClientType.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciadorProdutoECliente.Enums
+{
+    public enum ClientType
+    {
+        Individual = 'I',   // Pessoa Física
+        LegalEntity = 'J'   // Pessoa Jurídica
+    }
+}
