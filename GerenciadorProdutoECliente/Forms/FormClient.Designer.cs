ce467fd12ec69e8454f29d0e878eafc98c0f9f48
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-18 18:54:27 -0300

    Start Project and add FormClient

---
+++ GerenciadorProdutoECliente/Forms/FormClient.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace GerenciadorProdutoECliente.Forms
+{
+    partial class FormClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtCpfCnpj = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtComplement = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtNeighborhood = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblNeighborhood = new System.Windows.Forms.Label();
+            this.lblComplement = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCpfCnpj = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.bntNew = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(270, 35);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(329, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(45, 123);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(275, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtCpfCnpj
+            // 
+            this.txtCpfCnpj.Location = new System.Drawing.Point(344, 123);
+            this.txtCpfCnpj.Name = "txtCpfCnpj";
+            this.txtCpfCnpj.Size = new System.Drawing.Size(119, 20);
+            this.txtCpfCnpj.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(45, 166);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(245, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(344, 166);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(48, 237);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(100, 20);
+            this.txtZipCode.TabIndex = 5;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(154, 237);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(100, 20);
+            this.txtStreet.TabIndex = 6;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(270, 237);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(39, 20);
+            this.txtNumber.TabIndex = 7;
+            // 
+            // txtComplement
+            // 
+            this.txtComplement.Location = new System.Drawing.Point(324, 237);
+            this.txtComplement.Name = "txtComplement";
+            this.txtComplement.Size = new System.Drawing.Size(100, 20);
+            this.txtComplement.TabIndex = 8;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(290, 287);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(100, 20);
+            this.txtState.TabIndex = 9;
+            // 
+            // txtNeighborhood
+            // 
+            this.txtNeighborhood.Location = new System.Drawing.Point(48, 287);
+            this.txtNeighborhood.Name = "txtNeighborhood";
+            this.txtNeighborhood.Size = new System.Drawing.Size(100, 20);
+            this.txtNeighborhood.TabIndex = 9;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(171, 287);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 10;
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(290, 268);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(21, 13);
+            this.lblState.TabIndex = 11;
+            this.lblState.Text = "UF";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(171, 268);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(40, 13);
+            this.lblCity.TabIndex = 12;
+            this.lblCity.Text = "Cidade";
+            // 
+            // lblNeighborhood
+            // 
+            this.lblNeighborhood.AutoSize = true;
+            this.lblNeighborhood.Location = new System.Drawing.Point(48, 267);
+            this.lblNeighborhood.Name = "lblNeighborhood";
+            this.lblNeighborhood.Size = new System.Drawing.Size(34, 13);
+            this.lblNeighborhood.TabIndex = 13;
+            this.lblNeighborhood.Text = "Bairro";
+            // 
+            // lblComplement
+            // 
+            this.lblComplement.AutoSize = true;
+            this.lblComplement.Location = new System.Drawing.Point(324, 218);
+            this.lblComplement.Name = "lblComplement";
+            this.lblComplement.Size = new System.Drawing.Size(71, 13);
+            this.lblComplement.TabIndex = 14;
+            this.lblComplement.Text = "Complemento";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(270, 219);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(44, 13);
+            this.lblNumber.TabIndex = 15;
+            this.lblNumber.Text = "Numero";
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.Location = new System.Drawing.Point(154, 216);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(27, 13);
+            this.lblStreet.TabIndex = 16;
+            this.lblStreet.Text = "Rua";
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.AutoSize = true;
+            this.lblZipCode.Location = new System.Drawing.Point(49, 217);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(28, 13);
+            this.lblZipCode.TabIndex = 17;
+            this.lblZipCode.Text = "CEP";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(45, 104);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(82, 13);
+            this.lblName.TabIndex = 18;
+            this.lblName.Text = "Nome Completo";
+            // 
+            // lblCpfCnpj
+            // 
+            this.lblCpfCnpj.AutoSize = true;
+            this.lblCpfCnpj.Location = new System.Drawing.Point(341, 104);
+            this.lblCpfCnpj.Name = "lblCpfCnpj";
+            this.lblCpfCnpj.Size = new System.Drawing.Size(59, 13);
+            this.lblCpfCnpj.TabIndex = 19;
+            this.lblCpfCnpj.Text = "CPF/CNPJ";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(42, 150);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 20;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(341, 150);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(49, 13);
+            this.lblPhone.TabIndex = 21;
+            this.lblPhone.Text = "Telefone";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(627, 35);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 22;
+            this.btnSearch.Text = "Busar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(270, 19);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(193, 13);
+            this.lblSearch.TabIndex = 23;
+            this.lblSearch.Text = "Insira Nome, CPF ou CNPJ para busca:";
+            // 
+            // bntNew
+            // 
+            this.bntNew.Location = new System.Drawing.Point(76, 347);
+            this.bntNew.Name = "bntNew";
+            this.bntNew.Size = new System.Drawing.Size(75, 23);
+            this.bntNew.TabIndex = 24;
+            this.bntNew.Text = "Novo";
+            this.bntNew.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(171, 346);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 25;
+            this.btnUpdate.Text = "Atualizar";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(270, 346);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 26;
+            this.btnDelete.Text = "Deletar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // FormClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.bntNew);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblCpfCnpj);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblComplement);
+            this.Controls.Add(this.lblNeighborhood);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtNeighborhood);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtComplement);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtCpfCnpj);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "FormClient";
+            this.Text = "FormClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCpfCnpj;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtComplement;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtNeighborhood;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblNeighborhood;
+        private System.Windows.Forms.Label lblComplement;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblStreet;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCpfCnpj;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Button bntNew;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
