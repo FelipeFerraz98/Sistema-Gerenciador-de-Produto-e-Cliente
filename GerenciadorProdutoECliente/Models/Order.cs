36b49c2929a19589d8f61958fcfd9d5950ea380d
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-19 16:15:03 -0300

    Add OrderItem and Order

---
+++ GerenciadorProdutoECliente/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GerenciadorProdutoECliente.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }              // ID único do pedido
+        public int ClientId { get; set; }         // ID do cliente que fez o pedido
+        public DateTime OrderDate { get; set; }  // Data do pedido
+        public DateTime? CompletionDate { get; set; } // Data de conclusão do pedido (null se não finalizado)
+        public bool Canceled { get; set; }       // Status de cancelamento do pedido
+        public bool Finalized { get; set; }      // Status de finalização do pedido
+        public decimal TotalAmount { get; set; } // Valor total do pedido
+        public Client Client { get; set; }        // Cliente associado ao pedido
+        public List<OrderItem> OrderItems { get; set; } // Lista de itens do pedido
+
+        // Construtor
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
+
+        // Método para atualizar o valor total do pedido
+        public void UpdateTotalAmount()
+        {
+            TotalAmount = 0;
+            foreach (var item in OrderItems)
+            {
+                TotalAmount += item.TotalValue;
+            }
+        }
+    }
+}
---
+++ GerenciadorProdutoECliente/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GerenciadorProdutoECliente.Models
+{
+    public class OrderItem
+    {
+        public int OrderId { get; set; }         // ID do pedido ao qual o item pertence
+        public int ProductId { get; set; }       // ID do produto que foi comprado
+        public int Quantity { get; set; }        // Quantidade do produto no pedido
+        public decimal UnitPrice { get; set; }   // Preço unitário do produto no momento da venda
+        public decimal TotalValue { get; set; }  // Valor total do item (quantidade * preço unitário)
+        public Product Product { get; set; }     // Produto associado ao item
+
+        // Construtor
+        public OrderItem()
+        {
+        }
+
+        // Método para calcular o valor total do item (baseado na quantidade e preço unitário)
+        public void CalculateTotalValue()
+        {
+            TotalValue = Quantity * UnitPrice;
+        }
+    }
+}
