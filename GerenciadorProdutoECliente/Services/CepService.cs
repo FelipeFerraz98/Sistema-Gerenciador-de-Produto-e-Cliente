f8dda3128ead72a80ea3d00caa0bf470a09cdace
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-20 19:04:50 -0300

    Added CepService

---
+++ GerenciadorProdutoECliente/Services/CepService.cs
@@ -0,0 +1,22 @@
+﻿using GerenciadorProdutoECliente.Models;
+using GerenciadorProdutoECliente.Repositories;
+using System.Threading.Tasks;
+
+namespace GerenciadorProdutoECliente.Services
+{
+    public class CepService
+    {
+        private readonly CepRepository _cepRepository;
+
+        public CepService()
+        {
+            _cepRepository = new CepRepository(); // Inicializa o Repository
+        }
+
+        // Método para consultar o CEP e retornar os dados do endereço
+        public async Task<CepResponse> ConsultCepAsync(string cep)
+        {
+            return await _cepRepository.GetCepAsync(cep); // Chama o Repository para pegar os dados
+        }
+    }
+}
