ac0f8160b9b2c4e5e8da3e459aec144642e69ebc
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-19 22:09:36 -0300

    Add CashMovementRepository

---
+++ GerenciadorProdutoECliente/Repositories/CashMovementRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Npgsql;
+using GerenciadorProdutoECliente.Models;
+using GerenciadorProdutoECliente.Data;
+using GerenciadorProdutoECliente.Enums;
+
+namespace GerenciadorProdutoECliente.Repositories
+{
+    public class CashMovementRepository
+    {
+        // Método para adicionar uma movimentação de caixa
+        public bool AddCashMovement(CashMovement cashMovement)
+        {
+            using (NpgsqlConnection connection = DBConnection.OpenConnection())
+            {
+                string query = @"
+                    INSERT INTO caixa_pedidos (pedido_id, pago, pagar, forma_pagamento)
+                    VALUES (@OrderId, @Paid, @AmountDue, @PaymentMethod);";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@OrderId", cashMovement.OrderId);
+                    command.Parameters.AddWithValue("@Paid", cashMovement.Paid);
+                    command.Parameters.AddWithValue("@AmountDue", cashMovement.AmountDue);
+                    command.Parameters.AddWithValue("@PaymentMethod", cashMovement.PaymentMethod.ToString()); // Enum convertido para string
+
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        // Método para obter uma movimentação de caixa pelo ID do pedido
+        public CashMovement GetCashMovementByOrderId(int orderId)
+        {
+            using (NpgsqlConnection connection = DBConnection.OpenConnection())
+            {
+                string query = @"
+                    SELECT id, pedido_id, pago, pagar, forma_pagamento
+                    FROM caixa_pedidos
+                    WHERE pedido_id = @OrderId;";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@OrderId", orderId);
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new CashMovement
+                            {
+                                Id = reader.GetInt32(0),
+                                OrderId = reader.GetInt32(1),
+                                Paid = reader.GetDecimal(2),
+                                AmountDue = reader.GetDecimal(3),
+                                PaymentMethod = (PaymentMethod)Enum.Parse(typeof(PaymentMethod), reader.GetString(4)) // Convertendo de string para enum
+                            };
+                        }
+                    }
+                }
+            }
+            return null; // Retorna null se não encontrar movimentação
+        }
+    }
+}
