279ae74587a32405b6f7824d8148e517c32690bf
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-20 18:51:43 -0300

    Added CepResponse Model

---
+++ GerenciadorProdutoECliente/Models/CepResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace GerenciadorProdutoECliente.Models
+{
+    public class CepResponse
+    {
+        // O CEP que foi consultado
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        // Logradouro (Rua)
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        // Bairro
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        // Localidade (Cidade)
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+
+        // UF (Estado)
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+    }
+}
