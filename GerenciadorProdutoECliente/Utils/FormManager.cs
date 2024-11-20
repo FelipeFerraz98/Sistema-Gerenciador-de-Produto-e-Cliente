8d147c2990f7b581c060f514cc47492ef3dad9ad
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-20 18:10:57 -0300

    Added FormManeger Util

---
+++ GerenciadorProdutoECliente/Utils/FormManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+public static class FormManager
+{
+    // Método para trocar de formulários com Dispose no anterior
+    public static void SwitchForm(Form currentForm, Form newForm)
+    {
+        if (currentForm != null && currentForm.Visible)
+        {
+            currentForm.Hide(); // Esconde o formulário atual
+            currentForm.Dispose(); // Libera recursos do formulário atual
+        }
+
+        // Exibe o novo formulário
+        newForm.Show();
+    }
+}
