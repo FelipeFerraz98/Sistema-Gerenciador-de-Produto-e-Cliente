743549f368f5c2864b5a25f1350c79420f30a21a
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-19 21:32:30 -0300

    Add Enum PaymentMethod

---
+++ GerenciadorProdutoECliente/Enums/PaymentMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciadorProdutoECliente.Enums
+{
+    public enum PaymentMethod
+    {
+        Cash,              // Pagamento por dinheiro
+        Pix,               // Pagamento por Pix
+        DebitCard,         // Pagamento por Cartão de Débito
+        CreditCard         // Pagamento por Cartão de Crédito
+    }
+}
