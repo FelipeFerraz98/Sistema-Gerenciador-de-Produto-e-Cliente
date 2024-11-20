96ada345fd76e999673bc7c44878af1e9ddc205d
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-11-20 19:00:12 -0300

    Added CepRepository

---
+++ GerenciadorProdutoECliente/Repositories/CepRepository.cs
@@ -0,0 +1,35 @@
+﻿using GerenciadorProdutoECliente.Data;
+using GerenciadorProdutoECliente.Models;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace GerenciadorProdutoECliente.Repositories
+{
+    public class CepRepository
+    {
+        private readonly CepAPI _cepAPI;
+
+        public CepRepository()
+        {
+            _cepAPI = new CepAPI(); // Instancia a classe CepAPI para fazer a requisição
+        }
+
+        // Método para consultar o CEP
+        public async Task<CepResponse> GetCepAsync(string cep)
+        {
+            try
+            {
+                // Faz a requisição à API ViaCEP
+                string jsonResponse = await _cepAPI.GetCepDataAsync(cep);
+
+                // Desserializa a resposta JSON para um objeto CepResponse
+                return JsonConvert.DeserializeObject<CepResponse>(jsonResponse);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error fetching CEP data: {ex.Message}");
+            }
+        }
+    }
+}
